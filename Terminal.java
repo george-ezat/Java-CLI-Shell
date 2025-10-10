3cce995546579bf50b949a6549bb563b68dd11ea
george-ezat <e.georgeezat@gmail.com>
2025-10-10 18:11:05 +0300

    Implement rmdir command and enhance input parsing in Terminal

--- Terminal.java
+++ Terminal.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.File;
 
 // ==============================================
 
@@ -10,6 +11,15 @@ class Parser {
 
     // This method will divide the input into commandName and args
     public boolean parse(String input) {
+        String[] input_parts = input.trim().split("\\s+");
+
+        if (input_parts.length > 0) {
+            commandName = input_parts[0];
+            args = new String [input_parts.length - 1];
+            System.arraycopy(input_parts, 1, args, 0, input_parts.length - 1);
+            return true;
+        }
+
         return false;
     }
 
@@ -32,18 +42,75 @@ class Parser {
 public class Terminal {
     Parser parser;
 
-    // Implement each command in a method
-    // public String pwd() {}
+    // ------------------------------------------
+
+    public void rmdir(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Error: Invalid Args length!");
+            return;
+        }
+
+        String argument = args[0];
+        File current_directory = new File(System.getProperty("user.dir"));
+        File[] filesAndDirectories = current_directory.listFiles();
+
+        if ("*".equals(argument)) {
+            for (File item : filesAndDirectories) {
+                if (item.isDirectory() && item.listFiles().length == 0) {
+                    item.delete();
+                }
+            }
+        } else {
+            File directory = new File(argument);
+            if (directory.exists()) {
+                if (directory.isDirectory()) {
+                    if (directory.listFiles().length == 0) {
+                        directory.delete();
+                    } else {
+                        System.out.println("Error: Directory is not empty!");
+                    }
+                } else {
+                    System.out.println("Error: '" + argument + "'' is a file, not a directory!");
+                }
+            } else
+                System.out.println("Error: Directory does not exists");
+        }
+    }
 
     // ------------------------------------------
 
     // This method will choose the suitable command method to be called
     public void chooseCommandAction() {
+        switch (parser.commandName) {
+            case "rmdir":
+                rmdir(parser.args);
+                break;
+            default:
+                System.out.println("Error: Command not found!");
+        }
     }
 
     // ------------------------------------------
 
     public static void main(String[] args) {
+        Terminal terminal = new Terminal();
+        Scanner in = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            System.out.printf("%s $ ", System.getProperty("user.dir"));
+            input = in.nextLine();
+
+            if ("exit".equalsIgnoreCase(input.trim())) {
+                break;
+            }
+
+            terminal.parser = new Parser();
+            if (terminal.parser.parse(input)) {
+                terminal.chooseCommandAction();
+            }
+        }
+        in.close();
     }
 }
 
